8e025de5ea64fd3735a735878eac0e09733f11e4
kvngdre <kennedyugo2@gmail.com>
2024-09-09 20:08:00 +0100

    add notification types

---
+++ src/domain/notification/email-notification.ts
@@ -0,0 +1,10 @@
+export class EmailChannel {
+  constructor(
+    public readonly alias: string,
+    public readonly from: string,
+    public readonly to: string,
+    public readonly subject: string,
+    public readonly body: string,
+    public readonly attachments?: { filename: string; content: Buffer }[]
+  ) {}
+}
